d08206b9e4c15104f103442ec4af4acf01fabcae
sebastian <sebastian1995z@gmail.com>
2023-06-29 23:04:28 -0400

    commit_2

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { engine } = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.engine('handlebars', engine(
+    {layoutsDir:__dirname + '/views'
+    }
+));
+
+app.get('/', (req, res) => {
+    res.render('main.handlebars')
+});
+
+app.listen(port, () => {
+    console.log(`El servidor está inicializado en el puerto ${port}`);
+});
+
+/* codigo maria
+const { engine } = require('express-handlebars');
+
+const PORT = 3000;
+// Motor de plantillas
+
+app.engine('handlebars', engine());
+
+app.set('view engine', 'handlebars');
+
+app.set('views', './views');
+
+*/
+
