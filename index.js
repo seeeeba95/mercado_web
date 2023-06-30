666268f8e1de0d0327f5b6a2287563a33eb1e633
Víctor Araya <victor.d.araya@gmail.com>
2023-06-30 19:48:07 -0400

    bootstrapt

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const HOST = 'http://localhost';
+const PORT = 3000;
 
 const app = express();
-const port = 3000;
 
-app.engine('handlebars', engine({ layoutsDir: __dirname + '/views' }));
+// CONFIGURACION HANDLEBARS
+/* 1: Establece Handlebars como el motor de plantillas con la extensión .hbs y define el directorio de layouts que esta en /views.
+      Antes: app.engine('handlebars', engine({ layoutsDir: __dirname + '/views' }));.
+   2: Establece el motor de plantillas handlebars como predeterminado.
+   PD: set() es un metodo de express que permite establecer valores de configuracion. engine() es un metodo de handlebars que permite definir un motor (de plantillas?). */
+app.engine('hbs', engine({ extname: '.hbs', layoutsDir: __dirname + '/views' })); // 1
+app.set('view engine', 'hbs'); // 2
 
+// MIDDLEWARES
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+
+// ROUTES
 app.get('/', (req, res) => {
-	res.render('main.handlebars');
+	res.render('main');
 });
 
-app.listen(port, () => {
-	console.log(`El servidor está inicializado en el puerto ${port}`);
+// app.get('/user/:id', (req, res) => {
+// 	const user = req.params.user;
+// 	res.render('main', { user });
+// });
+
+app.listen(PORT, () => {
+	console.log(`El servidor está inicializado en  ${HOST}:${PORT}`);
 });
-
-/* codigo maria
-const { engine } = require('express-handlebars');
-
-const PORT = 3000;
-// Motor de plantillas
-
-app.engine('handlebars', engine());
-
-app.set('view engine', 'handlebars');
-
-app.set('views', './views');
-
-*/
